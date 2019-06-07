44819f0d08310db261f9d726e9c64ec3a144a0fe
Erik Myers <e.r.myers15@gmail.com>
2019-06-07 11:26:28 -0700

    add branch for years of experience between zero and three years

--- js/scripts.js
+++ js/scripts.js
@@ -57,17 +57,72 @@ console.log('jquery is enabled');
     } else if (years === 0 && ending === 'front' && building === 'cybersecurity' && companies === 'startup') {
       $('.languages #react').show();
       console.log('react selected');
-    } else if (years === 0 && ending === 'back' && building === 'cybersecurity' && companies === 'startup') {
+    } else if (years === 0 && ending === 'back' && building === 'cybersecurity' && companies === 'big') {
+      $('.languages #react').show();
+      console.log('react selected');
+    } else if (years === 0 && ending === 'back' && building === 'cybersecurity' && companies === 'gov') {
       $('.languages #react').show();
       console.log('react selected');
     } else if (years === 0 && ending === 'back' && building === 'cybersecurity' && companies === 'startup') {
       $('.languages #react').show();
       console.log('react selected');
-    } else if (years === 0 && ending === 'back' && building === 'cybersecurity' && companies === 'startup') {
+    } else if (years > 0 && years < 3 && ending === 'front' && building === 'apps' && companies === 'big') {
+      $('.languages #name #python').show();
+      console.log('python selected');
+    } else if (years === 0 && years < 3 && ending === 'front' && building === 'apps' && companies === 'gov') {
+      $('.languages #python').show();
+      console.log('javascript selected');
+    } else if (years > 0 && years < 3 && ending === 'front' && building === 'apps' && companies === 'startup') {
+      $('.languages #react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'back' && building === 'apps' && companies === 'big') {
+      $('.languages #react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'back' && building === 'apps' && companies === 'gov') {
+      $('.languages #react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'back' && building === 'apps' && companies === 'startup') {
+      $('.languages #react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'front' && building === 'data-mines' && companies === 'big') {
+      $('.languages #javascript').show();
+      console.log('javascript selected');
+    } else if (years > 0 && years < 3 && ending === 'front' && building === 'data-mines' && companies === 'gov') {
+      $('.languages #react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'front' && building === 'data-mines' && companies === 'startup') {
+      $('.languages#react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'back' && building === 'data-mines' && companies === 'big') {
+      $('.languages#react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'back' && building === 'data-mines' && companies === 'gov') {
+      $('.languages#react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'back' && building === 'data-mines' && companies === 'startup') {
+      $('.languages#react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'front' && building === 'cybersecurity' && companies === 'big') {
+      $('.languages #react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'front' && building === 'cybersecurity' && companies === 'gov') {
+      $('.languages #react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'front' && building === 'cybersecurity' && companies === 'startup') {
+      $('.languages #react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'back' && building === 'cybersecurity' && companies === 'big') {
+      $('.languages #react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'back' && building === 'cybersecurity' && companies === 'gov') {
+      $('.languages #react').show();
+      console.log('react selected');
+    } else if (years > 0 && years < 3 && ending === 'back' && building === 'cybersecurity' && companies === 'startup') {
       $('.languages #react').show();
       console.log('react selected');
 
-  
+
+
     } else {
       $('.languages #english').show();
       console.log('english selected');
